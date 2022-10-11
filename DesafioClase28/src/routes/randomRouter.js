85b11b10f92cc28a80f21747fb09b26dea27a010
FedericoOlguin <fede_olguin_10@outlook.com.ar>
2022-10-11 12:25:45 -0300

    Capturas

--- DesafioClase28/src/routes/randomRouter.js
+++ DesafioClase28/src/routes/randomRouter.js
@@ -1,12 +1,18 @@
 import { Router } from "express";
-
+import { fork } from "child_process"
+import __dirname from "../utils.js";
 const router = Router()
 
 
 
-router.get("/", async (req, res) => {
-    console.log(req.query.cant);
-    res.send(req.query.cant)
+router.get("/", (req, res) => {
+    // console.log(req.query.cant);
+    const cant = req.query.cant
+    const child = fork(__dirname + "/config/calculoRandoms.js")
+    child.send(cant + "")
+    child.on("message", val => {
+        res.json({ response: val })
+    })
 })
 
 
